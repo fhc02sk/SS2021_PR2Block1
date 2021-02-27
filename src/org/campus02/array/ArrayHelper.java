7477b4032b7b19724dc919c985b99895ccacefe7
Stephan Kochauf <stephan.kochauf@lv.campus02.at>
2021-02-27 11:45:51 +0100

    Initial commit

---
+++ src/org/campus02/array/ArrayHelper.java
@@ -0,0 +1,12 @@
+package org.campus02.array;
+
+public class ArrayHelper {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void arrangeOrder(int[] array){
+        
+    }
+}
